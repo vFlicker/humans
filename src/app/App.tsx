a85365869a2a1ee77a663f5480b62980a149f41b
Vladislav <flickervladislav@gmail.com>
2023-07-04 18:54:44 +0300

    init: setup new project

---
+++ src/app/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from '~/components/Button';
+import { Title } from '~/components/Title';
+
+export function App(): JSX.Element {
+  return (
+    <div>
+      <Title />
+      <Button />
+    </div>
+  );
+}
---
+++ src/components/Title/Title.tsx
@@ -0,0 +1,3 @@
+export function Title() {
+  return <h1>Title</h1>;
+}
